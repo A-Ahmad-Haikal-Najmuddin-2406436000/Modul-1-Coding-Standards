630d037a56cacf62496e63c96b81a74c1e091dfa
Kallln <ahmad.haikal41@ui.ac.id>
2026-02-10 15:52:54 +0700

    Change variable type of id from string to UUID

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -3,9 +3,11 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Setter;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter @Setter
 public class Product {
-    private String productid;
+    private UUID productid;
     private String productName;
     private int productQuantity;
 
