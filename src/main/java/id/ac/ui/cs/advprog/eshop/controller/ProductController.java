e64b06c06bc09b9af7ec416fc9fc069cf1f02a69
Kallln <ahmad.haikal41@ui.ac.id>
2026-02-24 13:46:46 +0700

    Fix issues SonarCloud

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -16,8 +16,12 @@ import java.util.UUID;
 @RequestMapping("/product")
 public class ProductController {
 
-    @Autowired
-    private ProductService service;
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
 
     @GetMapping("/create")
     public String createProductPage(Model model) {
